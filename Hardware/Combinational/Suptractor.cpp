bb592dff14eb16728faa8c5a81c2d6d9df5e57b7
Daniel Schranzhofer <daniel.schranzhofer@ms.bulme.at>
2025-11-26 13:11:14 +0100

    Suptractor is finished

--- Hardware/Combinational/Suptractor.cpp
+++ Hardware/Combinational/Suptractor.cpp
@@ -1,5 +1,21 @@
-//
-// Created by Daniel on 26.11.2025.
-//
+#include "HalfAdder.h"
+#include "Subtractor.h"
+#include "../Logic/OR.h"
+#include <vector>
 
-#include "Suptractor.h"
+void FullSubtractor::sub(unsigned int Bin, unsigned int A, unsigned int B) {
+    XOR xorGate;
+    AND andGate;
+    OR orGate;
+
+    unsigned int A_xor_B = xorGate.CalcXOR(A, B);
+    C = xorGate.CalcXOR(A_xor_B, Bin);
+
+    unsigned int borrow1 = andGate.CalcAND((~A) & 1, B);
+    unsigned int borrow2 = andGate.CalcAND(A_xor_B, Bin);
+
+    S = orGate.CalcOR(borrow1, borrow2);
+}
+
+unsigned int Subtractor::getC() const { return OutputC; }
+unsigned int Subtractor::getS() const { return OutputS; }
--- Hardware/Combinational/Suptractor.h
+++ Hardware/Combinational/Suptractor.h
@@ -1,13 +1,19 @@
-//
-// Created by Daniel on 26.11.2025.
-//
+#ifndef SUBTRACTOR_H
+#define SUBTRACTOR_H
 
-#ifndef Z80___CPU_SUPTRACTOR_H
-#define Z80___CPU_SUPTRACTOR_H
+#include <vector>
 
+class Subtractor {
+public:
+    Subtractor() : OutputC(0), OutputS(0) {};
+    void sub(unsigned int C, unsigned int input_1, unsigned int input_2);
 
-class Suptractor {
+    unsigned int getC() const;
+    unsigned int getS() const;
+
+private:
+    unsigned int OutputS;
+    unsigned int OutputC;
 };
 
-
-#endif //Z80___CPU_SUPTRACTOR_H
+#endif
