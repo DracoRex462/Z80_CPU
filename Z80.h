23105b6730f4277197fec26e89cd6061231bfac0
Daniel Schranzhofer <daniel.schranzhofer@ms.bulme.at>
2025-11-27 17:40:39 +0100

    Z80.5 first steps

--- Z80.h
+++ Z80.h
@@ -1,8 +1,15 @@
 #ifndef Z80___CPU_Z80_H
 #define Z80___CPU_Z80_H
 
+#include <vector>
 
 class Z80 {
+public:
+    Z80(std::vector<unsigned int>& InstructionRegister) : IR(InstructionRegister) {}
+    void Run(std::vector<unsigned int> IR);
+
+private:
+    std::vector<unsigned int> IR;
 };
 
 
