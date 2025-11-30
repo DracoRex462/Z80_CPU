769a64c8819c83a96f4adc350f19533b43c11619
Daniel Schranzhofer <daniel.schranzhofer@ms.bulme.at>
2025-11-30 18:12:48 +0100

    PC is finished

--- CPU/PC.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Daniel on 30.11.2025.
-//
-
-#include "PC.h"
--- CPU/PC.h
+++ CPU/PC.h
@@ -1,13 +1,17 @@
-//
-// Created by Daniel on 30.11.2025.
-//
-
-#ifndef Z80___CPU_PC_H
-#define Z80___CPU_PC_H
+#ifndef PC_H
+#define PC_H
 
 
-class PC {
+class PC
+{
+public:
+    PC() : counter(0x1000) {};
+    void setCounter(unsigned int counter) : counter(counter) {};
+    unsigned int  getCounter() { return counter; }
+
+private:
+    unsigned int counter;
 };
 
 
-#endif //Z80___CPU_PC_H
+#endif
