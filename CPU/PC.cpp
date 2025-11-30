d9fb7f90f5b27ef9a49f50a5da20b3507f8de946
Daniel Schranzhofer <daniel.schranzhofer@ms.bulme.at>
2025-11-30 18:09:08 +0100

    Add Programm Counter to project

---
+++ CPU/PC.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Daniel on 30.11.2025.
+//
+
+#include "PC.h"
---
+++ CPU/PC.h
@@ -0,0 +1,13 @@
+//
+// Created by Daniel on 30.11.2025.
+//
+
+#ifndef Z80___CPU_PC_H
+#define Z80___CPU_PC_H
+
+
+class PC {
+};
+
+
+#endif //Z80___CPU_PC_H
