b95e63e0083c09b51efd474202d6c0c34534adf1
Daniel Schranzhofer <daniel.schranzhofer@ms.bulme.at>
2025-11-30 17:46:12 +0100

    Register class is finished

--- Register.h
+++ Register.h
@@ -1,13 +1,18 @@
-//
-// Created by Daniel on 30.11.2025.
-//
+#ifndef REGISTER_H
+#define REGISTER_H
 
-#ifndef Z80___CPU_REGISTER_H
-#define Z80___CPU_REGISTER_H
+#include <cstdint>
+#include <vector>
 
+class Register
+{
+public:
+    void write(unsigned int addr, unsigned int value) { R[addr].push_back(value); };
+    std::vector<uint8_t>& read(unsigned int addr) { return R[addr]; }
 
-class Register {
+private:
+    std::vector<uint8_t> R[7];
 };
 
 
-#endif //Z80___CPU_REGISTER_H
+#endif
