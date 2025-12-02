b3e59ff24175ce30649e7047520c0a7bc44b19a8
Daniel Schranzhofer <daniel.schranzhofer@ms.bulme.at>
2025-12-02 19:25:02 +0100

    Update Run method

--- Z80.cpp
+++ Z80.cpp
@@ -1,20 +1,22 @@
 #include "Z80.h"
+#include "CPU/ControlUnit.h"
 
 void Z80::Run()
 {
     bool approved = ApproveVector();
-    if (approved == true)
+    if (approved)
     {
-
+        ControlUnit* Control = new ControlUnit(IR);
+        Control->decode_and_execute();
     }
-    else
-        return 0;
+    else { throw; }
 }
+
 
 int Z80::ApproveVector()
 {
     for(int pc = 0; pc < IR.size(); ++pc) {
-        if(IR[pc] == 0x0000) return true;
+        if(IR[pc] == 0x76) return true;
     }
     return false;
 }
--- Z80.h
+++ Z80.h
@@ -7,7 +7,7 @@
 class Z80 {
 public:
     Z80(const std::vector<unsigned int>& InstructionRegister) : IR(InstructionRegister) {}
-    void Run(std::vector<unsigned int> IR);
+    void Run();
 
     void PrintRegister();
     void PrintPCCounter();
