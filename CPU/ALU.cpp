75797d2521cbe4029bd7770e6e61e31b0d2a047b
Daniel Schranzhofer <daniel.schranzhofer@ms.bulme.at>
2025-12-02 19:07:03 +0100

    Add HL to methods in class ALU

--- CPU/ALU.cpp
+++ CPU/ALU.cpp
@@ -1,33 +1,46 @@
 #include "ALU.h"
+#include "PC.h"
 #include "../Memory/Register.h"
 #include "../Memory/Memory.h"
 
 #include "../Hardware/Combinational/FullAdder8.h"
 #include "../Hardware/Combinational/FullSubtractor8.h"
 
-void ALU::execute(unsigned int xField, unsigned int yField)
+
+void ALU::execute(unsigned int xField, unsigned int yField, uint16_t HL)
 {
     switch(xField)
     {
-        case 0x9: //SUP
-            loadA(executeSUP(yField));
-        case 0x8: //ADD
-            loadA(executeADD(yField));
-        case 0x7: //LD
-            break;
+        case 0x9: loadA(executeSUP(yField));  break; //SUP
+        case 0x8: loadA(executeADD(yField));  break; //ADD
+        case 0x7: break; //LD
     }
 }
+
+
+
+int ALU::controlYField(unsigned int yField)
+{
+    return (yField <= 5) ? 0 : (yField == 6 ? 1 : 2);
+}
+
 void ALU::loadA(unsigned int value)
 {
     Register R;
     R.write(0, value);
 };
 
-unsigned int ALU::executeADD(unsigned int yField)
+unsigned int ALU::executeADD(unsigned int yField, uint16_t HL)
 {
     Register R;
     FullAdder8 Adder;
-    Adder.FullSup8(R.read(0), yField);
+
+    switch(controlYField(yField))
+    {
+        case 0: Adder.add(R.read(0), yField); break;
+        case 1: Adder.add(R.read(0), HL); break; // TODO
+        case 2: Adder.add(R.read(0), R.read(0)); break;
+    }
     return Adder.getSumADDR();
 }
 
@@ -35,7 +48,13 @@ unsigned int ALU::executeSUP(unsigned int yField)
 {
     Register R;
     FullSuptractor8 Subtractor;
-    Subtractor.FullSup8(R.read(0), yField);
+
+    switch(controlYField(yField))
+    {
+        case 0: Subtractor.sub(R.read(0), yField); break;
+        case 1: Subtractor.sub(R.read(0), HL); break; // TODO
+        case 2: Subtractor.sub(R.read(0), R.read(0)); break;
+    }
     return Subtractor.getSumSUP();
 }
 
--- CPU/ALU.h
+++ CPU/ALU.h
@@ -1,14 +1,17 @@
 #ifndef ALU_H
 #define ALU_H
 
+#include <cstdint>
+
 class ALU
 {
 public:
     ALU() {};
-    void execute(unsigned int xField, unsigned int yField);
+    void execute(unsigned int xField, unsigned int yField, uint16_t HL);
 
 private:
     void loadA(unsigned int value);
+    int controlYField(unsigned int yField);
     unsigned int executeADD(unsigned int yField);
     unsigned int executeSUP(unsigned int yField);
     unsigned int executeLD(unsigned int yField);
