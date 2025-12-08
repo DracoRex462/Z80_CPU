2259021f216084a10e5f274c8aedb0f4ef39b79c
Daniel Schranzhofer <daniel.schranzhofer@ms.bulme.at>
2025-12-08 16:51:49 +0100

    Update Add and Sub in ALU.cpp

--- CPU/ALU.cpp
+++ CPU/ALU.cpp
@@ -5,36 +5,29 @@
 #include "../Hardware/Combinational/FullAdder8.h"
 #include "../Hardware/Combinational/FullSubtractor8.h"
 
-// ----------------------------------------------------------------------------------
-
 void ALU::execute(unsigned int op, unsigned int yField, unsigned int zField, uint16_t HL)
 {
     switch(op)
     {
-        case 1:          executeLD(yField, HL);     break;      // LD
-        case 2:          executeADD(yField, HL);    break;      // ADD
-        case 3:          executeSUP(yField, HL);    break;      // SUP
+        case 1:          executeLD(yField, zField, HL);     break;      // LD
+        case 2:          executeADD(zField);                break;      // ADD
+        case 3:          executeSUP(zField);                break;      // SUP
     }
 }
-uint8_t ALU::executeADD(unsigned int zField, uint16_t HL)
+
+uint8_t ALU::executeADD(unsigned int zField)
 {
     Register reg;
-    Memory mem;
     FullAdder8 Adder;
-
-    if (zField == 6)    { Adder.add(reg.read(0), mem.read(HL)); }
-    else                { Adder.add(reg.read(0), reg.read(zField)); }
+    Adder.add(reg.read(0), reg.read(zField));
     return Adder.getSumADDR();
 }
 
 uint8_t ALU::executeSUP(unsigned int zField, uint16_t HL)
 {
     Register reg;
-    Memory mem;
     FullSubtractor8 Sub;
-
-    if (zField == 6)    { Sub.sub(reg.read(0), mem.read(HL)); }
-    else                { Sub.sub(reg.read(0), reg.read(zField)); }
+    Sub.sub(reg.read(0), reg.read(zField));
     return Sub.getSumSUP();
 }
 
