2fe3454427b42a41e702dfdcaa898da74b6fa130
Daniel Schranzhofer <daniel.schranzhofer@ms.bulme.at>
2025-12-08 16:14:46 +0100

    Clean up Alu.cpp slowly

--- CPU/ALU.cpp
+++ CPU/ALU.cpp
@@ -6,6 +6,7 @@
 #include "../Hardware/Combinational/FullAdder8.h"
 #include "../Hardware/Combinational/FullSubtractor8.h"
 
+// ----------------------------------------------------------------------------------
 
 void ALU::execute(uint8_t command , uint16_t HL)
 {
@@ -15,10 +16,11 @@ void ALU::execute(uint8_t command , uint16_t HL)
         case 0x4 ... 0x7:  executeLD(yField, HL); break;                  //LD
         case 0x8:          registerLD(executeADD(yField, HL), 0);  break; //ADD
         case 0x9:          registerLD(executeSUP(yField, HL), 0);  break; //SUP
-
     }
 }
 
+// ----------------------------------------------------------------------------------
+/*
 int ALU::controlYField(unsigned int yField)
 {
     return (yField <= 5) ? 0 : (yField == 6 ? 1 : 2);
@@ -29,25 +31,17 @@ void ALU::registerLD(unsigned int value, int reg)
     Register R;
     R.write(reg, value);
 };
+*/
 
-void ALU::createFields(uint8_t command)
-{
-    xField = (command >> 6) & 0x03;
-    yField = (command >> 3) & 0x07;
-    zField = command & 0x07;
-}
 
 unsigned int ALU::executeADD(unsigned int yField, uint16_t HL)
 {
-    Register R;
+    Register reg;
+    Memory mem;
     FullAdder8 Adder;
 
-    switch(controlYField(yField))
-    {
-        case 0: Adder.add(R.read(0), yField); break;
-        case 1: Adder.add(R.read(0), HL); break;
-        case 2: Adder.add(R.read(0), R.read(0)); break;
-    }
+    if (zField == 6)    { Adder.add(reg.read(0), mem.read(HL)); }
+    else                { Adder.add(reg.read(0), reg.read(zField)); }
     return Adder.getSumADDR();
 }
 
@@ -63,6 +57,16 @@ unsigned int ALU::executeSUP(unsigned int yField, uint16_t HL)
         case 2: Subtractor.sub(R.read(0), R.read(0)); break;
     }
     return Subtractor.getSumSUP();
+}
+
+
+// ----------------------------------------------------------------------------------------------------
+
+void ALU::createFields(uint8_t command)
+{
+    xField = (command >> 6) & 0x03;
+    yField = (command >> 3) & 0x07;
+    zField = command & 0x07;
 }
 
 void ALU::executeLD(uint8_t command, uint16_t HL)
