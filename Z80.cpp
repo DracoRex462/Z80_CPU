05b63c3b92c7a9fc18cde920b5801d35c0aa4f4c
Daniel Schranzhofer <daniel.schranzhofer@ms.bulme.at>
2025-12-09 11:12:42 +0100

    PrintPCCounter() method is finished

--- Z80.cpp
+++ Z80.cpp
@@ -1,5 +1,6 @@
 #include "Z80.h"
 #include "CPU/ControlUnit.h"
+#include "CPU/PC.h"
 
 #include <iostream>
 
@@ -12,7 +13,7 @@ void Z80::Run()
         Control->decode_and_execute();
     }
     else { throw; }
-};
+}
 
 int Z80::ApproveVector()
 {
@@ -20,11 +21,16 @@ int Z80::ApproveVector()
         if(IR[pc] == 0x76) return true;
     }
     return false;
-};
+}
 
 void Z80::PrintRegister() {
     Register R;
     std::cout << "Start" << std::endl;
     for (int i = 0; i < 7; ++i)
         std::cout << "Register " << i << ": " << R.read(i) << std::endl;
-};
+}
+
+void Z80::PrintPCCounter() {
+    PC counter;
+    std::cout << "PC Counter: " << counter.getCounter() << std::endl;
+}
